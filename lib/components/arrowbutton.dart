0deb8d61c78d91459b6ba187ae8c9c29bccfdefc
Muhammad Sarmad Nadeem <122588901+sarmaddevil1346@users.noreply.github.com>
2023-09-08 11:40:34 +0500

    created a project

---
+++ lib/components/arrowbutton.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+import '../config/colors.dart';
+import '../config/size.dart';
+
+
+class ArrowButton extends StatelessWidget {
+  final EdgeInsets margin;
+  final Widget icon;
+  ArrowButton({required this.margin, required this.icon});
+  @override
+  Widget build(BuildContext context) {
+    var height = SizeConfig.getHeight(context);
+    var width = SizeConfig.getWidth(context);
+    return Expanded(
+      child: Container(
+        margin: margin,
+        decoration: BoxDecoration(
+            color: AppColors.primaryWhite,
+            borderRadius: BorderRadius.circular(15),
+            boxShadow: AppColors.neumorpShadow),
+        alignment: Alignment.center,
+        child: icon,
+      ),
+    );
+  }
+}
